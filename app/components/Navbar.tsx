03b2954f907faaed435c3fa75411cacda991c5eb
Pratik <survasepratik1304@gmail.com>
2025-09-28 22:39:57 +0530

    navbar done

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between p-4'>
+      {/*Search Bar */}
+      <div className='hidden md:flex items-center rounded-full text-xs gap-2 ring-[1.5px] ring-gray-300 px-2'>
+        <Image src="/search.png" alt='search bar' height={20} width={20}/>
+        <input type="text" placeholder='Search...' className='w-[200px] p-2 bg-transparent outline-none'/>
+      </div>
+      {/*Icons and user button */}
+      <div className='flex items-center gap-6 justify-end w-full'>
+        <div className='relative bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+          <Image src="/message.png" alt='message' height={20} width={20}/>
+          {/* <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500'>1</div> */}
+          <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>1</div>
+          
+        </div>
+        <div className="  bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+        <Image src="/announcement.png" alt='announcement' height={20} width={20}/>
+        </div>
+        <div className='flex flex-col'>
+          <span className='text-xs leading-3 font-medium'>Peter Parker</span>
+          <span className='text-[10px] text-gray-500 text-center'>Admin</span>
+          
+
+        </div>
+        <Image src="/avatar.png" alt='' width={36} height={36} className='rounded-full'/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
